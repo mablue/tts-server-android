1dae4cb94359b563129b124be140ceba495397af
Masoud Azizi <mablue92@gmail.com>
2024-10-17 08:23:26 +0330

    Update build_gojni.sh

--- .github/scripts/build_gojni.sh
+++ .github/scripts/build_gojni.sh
@@ -1,9 +1,12 @@
+
+
+
 #!/bin/bash
 
 cd $GITHUB_WORKSPACE/lib-gojni-aar/tts-server-lib
 
-go install golang.org/x/mobile/cmd/gomobile
+go install golang.org/x/mobile/cmd/gomobile@latest
 gomobile init
-go get golang.org/x/mobile/bind
 gomobile bind -ldflags "-s -w" -v -androidapi=21
-# mv -f *.aar ../
+# Consider removing this line if using a build system
+mv -f *.aar ../
